58761440b319f331c8a7ce2a6f241d6232c328a5
MargaritaM12 <108419145+MargaritaM12@users.noreply.github.com>
2023-01-13 04:27:21 +0300

    fixed mistakes

--- 2.js
+++ 2.js
@@ -6,17 +6,11 @@ class Cat {
         this.comment = options.comment;
     }
 }
-
-const cat = new Cat({namepet: {}, nameowner: {}, contact: {}, comment: {}})
-
-console.log(cat);
-
+const cat = new Cat({namepet: {}, nameowner: {}, contact: {}, comment: {}});
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
-    document
-        .querySelector('input')
-        .value;
+    document.querySelector('input');.value.cat;
     console.log(input.value);
 })
